8f89fff0199ead1172e276b49a3a332e21471e1d
Anna A Dominiak <a.dominiak@primemodule.com>
2015-03-25 18:31:24 +0100

    IE support

--- js/drag.js
+++ js/drag.js
@@ -3,18 +3,19 @@
     var dragSrcEl = null;
 
     function handleDragStart(e) {
-      this.classList.add("is-dragged");
       dragSrcEl = this;
+      addClass(this, "is-dragged");  //jquery please! classList doesn't work in ie9
       e.dataTransfer.effectAllowed = 'move';
-      e.dataTransfer.setData('text/html', this.innerHTML);
+      e.dataTransfer.setData("text", e.target.id);
     }
 
     function handleDragEnd(e) {
-      this.classList.remove("is-dragged");
+      removeClass(this, 'is-dragged'); // this.classList.remove("is-dragged"); - does not work in ie9
     }
 
     function handleDragEnter(e) {
-      this.classList.add('over');
+      addClass(this, "over");
+      // this.classList.add('over');
     }
     function handleDragOver(e) {
       if (e.preventDefault) {
@@ -25,39 +26,59 @@
     }
 
     function handleDragLeave(e) {
-      this.classList.remove('over');
+      // this.classList.remove('over');
+      removeClass(this, 'over');
     }
 
     function handleDrop(e) {
+      if(e.preventDefault) {
+        e.preventDefault();
+      }
       if (e.stopPropagation) {
         e.stopPropagation();
       }
       if (dragSrcEl != this) {
-        dragSrcEl.innerHTML = this.innerHTML;
-        var div = document.createElement("DIV");
-        div.classList.add('dragable');
-        div.draggable = true;
-        attachEvents(div);
-        div.innerHTML = e.dataTransfer.getData('text/html');
         if (this.compareDocumentPosition(dragSrcEl) === 4){
-          this.parentElement.insertBefore(div,this);
+          this.parentElement.insertBefore(dragSrcEl,this);
         }
         else {
           if (this === this.parentElement.lastChildElement) {
-            console.log('last');
-            this.parentElement.appendChild(div);
+            this.parentElement.appendChild(dragSrcEl);
           }
           else {
-            this.parentElement.insertBefore(div, this.nextElementSibling);
+            this.parentElement.insertBefore(dragSrcEl, this.nextElementSibling);
           }
         }
-        this.parentElement.removeChild(dragSrcEl);
       }
-      this.classList.remove('over');
+      // this.classList.remove('over');
+      removeClass(this, 'over');
       return false;
     }
 
-    function attachEvents(element){
+    function removeClass(element, classToRemove) {
+      var classList = element.getAttribute("class").split(" ");
+      var finalClasses = [];
+      var i = classList.length;
+      while (i--) {
+        if (classList[i] !== classToRemove) {
+          finalClasses.push(classList[i]);
+        }
+      }
+      element.setAttribute("class", finalClasses.join(" "));
+    }
+
+    function addClass(element, classToAdd) {
+      var classList = element.getAttribute("class").split(" ");
+      var i = classList.length;
+      while (i--) {
+         if (classList[i] == classToAdd) {
+             return;
+         }
+      }
+      element.setAttribute("class", classList.join(" ")+" "+classToAdd);
+    }
+
+    function attachEvents(element) {
       element.addEventListener('dragstart', handleDragStart, false);;
       element.addEventListener('dragenter', handleDragEnter, false);
       element.addEventListener('dragover', handleDragOver, false);
